e1cd8865757beb7f78906d0cc26dbfc8c863f493
Patrick Lambert <dendory@gmail.com>
2015-06-29 15:27:01 -0400

    work on engine

--- cli.py
+++ cli.py
@@ -1,10 +1,60 @@
+#
+# Command line interface for Kara - (C) 2015 Patrick Lambert - Provided under the MIT License
+#
 import kara
 
-concepts = kara.load_concepts()
+concepts = {}
+concepts_name = ""
 
-kara.add_terms(concepts, kara._split_words("test me")) # Add terns
-kara.relate_terms(concepts, kara._split_words("hair stomach finger bone skin toe foot"), "body", "PART OF") # Define terms
-print(kara.list_terms(concepts)) # Print a list of terms
-kara.define_types(concepts) # Basic type definitions
-
-kara.save_concepts(concepts)
+while True:
+	q = input("> ")
+	words = kara.parse_query(q)
+	question = False
+	if len(q) > 1 and q[-1] == '?':
+		question = True
+	if (kara.is_a_term(concepts, words[0]) and len(words) == 1) or (words[0].lower() == 'define' and len(words) > 1): # Define a term
+		term = words[0]
+		if words[0].lower() == 'define':
+			term = words[1]
+		kara.add_terms(concepts, [term])
+		relations = kara.get_relations(concepts, term)
+		for r in relations:
+			print(term + " " + r['relation'] + ": " + r['name'])
+	elif words[0].lower() == 'load' and len(words) > 1: # Load a data set
+		concepts = kara.load_concepts(words[1] + ".json")
+		concepts_name = words[1]
+		print("Loaded: " + concepts_name)
+	elif words[0].lower() == 'save': # Save a data set
+		if concepts_name == "":
+			concepts_name = input("Enter data set name: ")
+		kara.save_concepts(concepts, concepts_name + ".json")
+		print("Saved: " + concepts_name)
+	elif words[0].lower() == 'list': # List concepts
+		if len(words) < 2 or words[1].lower() == 'concepts' or words[1].lower() == 'terms':
+			print(kara.get_terms(concepts))
+		elif words[1].lower() == 'relations' or words[1].lower() == 'keywords':
+			print(str(kara.relations))
+			print(str(kara.inverses))
+	elif words[0].lower() == 'quit' or words[0].lower() == 'exit': # Quit
+		if (concepts_name != "" and concepts != kara.load_concepts(concepts_name + ".json")) or (concepts_name == "" and concepts != {}):
+			if input("Data set modified. Quit without saving [y/n]? ").lower() == 'y':
+				quit(0) 
+		else:
+			quit(0)
+	elif question: # A question was asked
+		terms = kara.find_terms(concepts, words)
+		if len(terms) < 2:
+			print("Ambiguous question. Only the following term is defined: " + str(terms))
+		else:
+			print("[" + terms[0] + "] of [" + terms[1] + "]: " + kara.find_relation(concepts, terms[0], terms[1]))
+	else: # Try to define a relationship
+		found = False
+		for r in kara.relations:
+			if r in q:
+				print("[" + words[0] + "] " + r + " [" + words[-1] + "]")
+				kara.relate_term(concepts, str(words[0]), str(words[-1]), r)
+				found = True
+			if found:
+				break
+		if not found:
+			print("Unknown statement: " + str(words))
--- kara.py
+++ kara.py
@@ -1,14 +1,15 @@
 #
-# Simple word association script - (C) 2015 Patrick Lambert - Provided under the MIT License
+# Concepts engine - (C) 2015 Patrick Lambert - Provided under the MIT License
 #
 import json
 import time
 import re
 import string
 
-relations = ["GREATER THAN", "LESSER THAN", "PART OF", "HAS AS PART", "CONTAINED IN", "CONTAINS", "SAME AS", "IS A", "DEFINES"]
-inverses = ["LESSER THAN", "GREATER THAN", "HAS AS PART", "PART OF", "CONTAINS", "CONTAINED IN", "SAME AS", "DEFINES", "IS A"]
+relations = ["greater", "lesser", "part", "joins", "contained", "contains", "same", "is", "defines", "plurial", "singular"]
+inverses = ["lesser", "greater", "joins", "part", "contains", "contained", "same", "defines", "is", "singular", "plurial"]
 types = ["_number", "_person", "_good", "_bad"]
+ignore_terms = ["and", "or", "can", "you", "i", "how", "why", "when", "what", "which", "a", "the", "than", "of", "my", "your"]
 
 #
 # Utility functions
@@ -28,50 +29,50 @@ def _split_words(text): # Split sentence into words
 	text = ''.join(ch for ch in text if ch not in exclude)
 	return re.split(' |\n', text)
 
-def _log(concepts, event): # Log an event
-	for concept in concepts:
-		if concept["name"] == "_log":
-			concept["data"].append({"event": event, "time": _now()})
+def _basic_types(concepts, term): # Define basic known types
+	if _is_int(term): # Numbers
+		concepts[term]["relations"].append({"name": "_number", "relation": "is"})
+	if term == "me" or term == "kara": # Person
+		concepts[term]["relations"].append({"name": "_person", "relation": "is"})
 
 #
 # Main functions
 #
-def load_concepts(): # Load concepts from file or create blank structure
+def load_concepts(filename): # Load concepts from file or create blank structure
+	concepts = {}
 	try:
-		f = open("concepts.json", "r")
+		f = open(filename, "r")
 		concepts = json.loads(f.read())
 		f.close()
 		return concepts
 	except:
-		concepts = [{"name": "_log", "relations": [], "bias": 0, "data": [{"event": "Creation", "time": _now()}]}]
 		for t in types:
-			concepts.append({"name": t, "relations": [], "bias": 0, "data": []})
+			concepts[t] = {"relations": [], "bias": 0, "data": []}
 		return concepts
 	
-def save_concepts(concepts): # Save concepts back to file
-	f = open("concepts.json", "w")
+def save_concepts(concepts, filename): # Save concepts back to file
+	f = open(filename, "w")
 	f.write(json.dumps(concepts, sort_keys=True, indent=4))
 	f.close()
 
 def add_terms(concepts, terms): # Add terms to concepts
 	for term in terms:
 		if term != "":
-			found = False
-			for concept in concepts:
-				if str(term).lower() == concept["name"]:
-					found = True
-					concept["bias"] = concept["bias"] + 1  # Increase bias when we find a recurrence
-			if not found:
-				concepts.append({"name": str(term).lower(), "relations": [], "bias": 0, "data": []})
+			if str(term).lower() not in concepts:
+				concepts[str(term).lower()] = {"relations": [], "bias": 0, "data": []}
+				_basic_types(concepts, str(term).lower())
+			else:
+				concepts[str(term).lower()]["bias"] = concepts[str(term).lower()]["bias"] + 1  # Increase bias when we find a recurrence
 
-def list_terms(concepts): # List all terms
+def get_terms(concepts): # List all terms
 	results = []
-	for term in concepts:
-		results.append(term["name"])
+	for k in concepts.keys():
+		results.append(k)
+	results.sort()
 	return results
 
 def check_relation(relation): # Validate a relation
-	if str(relation).upper() in relations:
+	if str(relation).lower() in relations:
 		return True    
 	return False
 
@@ -79,23 +80,21 @@ def inverse_relation(relation): # Inverse a relation
 	return inverses[relations.index(relation)]
 
 def relate_term(concepts, term1, term2, relation): # Assign a relation between two term
-	add_terms(concepts, [term1, term2])
+	if not str(term1).lower() in concepts or not str(term2).lower() in concepts:
+		add_terms(concepts, [term1, term2])
 	if check_relation(relation):
-		for concept in concepts:
-			if concept["name"] == str(term1).lower():
-				found = False
-				for r in concept["relations"]:
-					if str(term2).lower() == r["name"]:
-						found = True
-				if not found:
-					concept["relations"].append({"name": str(term2).lower(), "relation": str(relation).upper()})
-			elif concept["name"] == str(term2).lower():
-				found = False
-				for r in concept["relations"]:
-					if str(term1).lower() == r["name"]:
-						found = True
-				if not found:
-					concept["relations"].append({"name": str(term1).lower(), "relation": inverse_relation(relation)})
+		found = False
+		for r in concepts[str(term1).lower()]["relations"]:
+			if str(term2).lower() == r["name"]:
+				found = True
+		if not found:
+			concepts[str(term1).lower()]["relations"].append({"name": str(term2).lower(), "relation": relation})
+		found = False
+		for r in concepts[str(term2).lower()]["relations"]:
+			if str(term1).lower() == r["name"]:
+				found = True
+		if not found:
+			concepts[str(term2).lower()]["relations"].append({"name": str(term1).lower(), "relation": inverse_relation(relation)})
 
 def relate_terms(concepts, terms, term, relation): # Relate multiple terms to one
 	add_terms(concepts, [term])
@@ -103,15 +102,36 @@ def relate_terms(concepts, terms, term, relation): # Relate multiple terms to on
 	for t in terms:
 		relate_term(concepts, t, term, relation)
 
-def add_type(concepts, term, mytype): # Add a basic type
-	if mytype in types:
-		for concept in concepts:
-			if concept["name"] == term.lower():
-				relate_term(concepts, term, mytype, "IS A")
+def get_relations(concepts, term): # Define a concept and its relations
+	if str(term).lower() in concepts:
+		return concepts[str(term).lower()]["relations"]
+	return []
 
-def define_types(concepts): # Define basic types
-	for concept in concepts:
-		if _is_int(concept["name"]): # Numbers
-			add_type(concepts, concept["name"], "_number")
-		if concept["name"] == "me" or concept["name"] == "kara": # Person
-			add_type(concepts, concept["name"], "_person")
+def parse_query(text): # Parse a query and return useful terms
+	words = _split_words(text)
+	for t in ignore_terms:
+		words = [x for x in words if x.lower() != t]
+	return words
+
+def is_a_term(concepts, term): # Check if a term is defined
+	if str(term).lower() in concepts:
+		return True
+	return False
+
+def find_terms(concepts, words): # find defined terms in a series of words
+	results = []
+	for word in words:
+		if str(word).lower() in concepts:
+			results.append(word)
+	return results
+
+def find_relation(concepts, term1, term2): # Find the relation between two terms
+	if not str(term1).lower() in concepts or not str(term2).lower() in concepts:
+		return "_undefined"
+	for r in concepts[str(term2).lower()]["relations"]: # 1st generation relation
+		if r["name"] == str(term1).lower():
+			return r["relation"]
+		for x in concepts[r["name"]]["relations"]: # 2nd generation relation
+			if x["name"] == str(term1).lower():
+				return r["name"]
+	return "_unknown"
